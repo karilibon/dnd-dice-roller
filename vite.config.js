d72247e563c11a2c894bbde7c9c90b073d93e833
karilibon <nikbon52ngg@gmail.com>
2025-12-12 23:35:29 +0300

    Fix: use CDN for dice-box on GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/dnd-dice-roller/',
+  base: './',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    rollupOptions: {
+      external: ['@3d-dice/dice-box'], // Не включать в бандл
+      output: {
+        globals: {
+          '@3d-dice/dice-box': 'DiceBox' // Использовать глобальную переменную
+        }
+      }
+    }
   }
 })
